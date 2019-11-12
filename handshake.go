75d8fd86c7e2789144b56cf5b4e8ef30635c9bf9
Till Klocke <till.klocke@gmail.com>
2019-11-12 16:16:20 +0100

    Added very simple interface implementation for ReadableHandshakeMessage and WriteableHandshakeMessage to be used in tests

--- handshake.go
+++ handshake.go
@@ -12,3 +12,48 @@ type WriteableHandshakeMessage interface {
 	WriteEncryptedSPublic(s []byte)
 	WriteEncryptedPayload(p []byte)
 }
+
+type SimplePayload struct {
+	epublic  []byte
+	espublic []byte
+	payload  []byte
+}
+
+func (s *SimplePayload) Reset() {
+	s.espublic = []byte{}
+	s.epublic = []byte{}
+	s.payload = []byte{}
+}
+
+func (s *SimplePayload) WriteEPublic(e []byte) {
+	s.epublic = e
+}
+
+func (p *SimplePayload) WriteEncryptedSPublic(s []byte) {
+	p.espublic = s
+}
+
+func (s *SimplePayload) WriteEncryptedPayload(p []byte) {
+	s.payload = p
+}
+
+func (s *SimplePayload) ReadEPublic() ([]byte, error) {
+	return s.epublic, nil
+}
+
+func (s *SimplePayload) ReadEncryptedSPublic() ([]byte, error) {
+	return s.espublic, nil
+}
+
+func (s *SimplePayload) ReadPayload() []byte {
+	return s.payload
+}
+
+func (s *SimplePayload) Serialize() []byte {
+	t := append(s.epublic, s.espublic...)
+	return append(t, s.payload...)
+}
+
+func (s *SimplePayload) Length() int {
+	return len(s.Serialize())
+}
