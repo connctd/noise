664ac0c4457561ad010240678bab749f4d56f5cc
Till Klocke <till.klocke@gmail.com>
2019-11-12 16:16:42 +0100

    Refactored tests so thag they make use of the new interface

--- noise_test.go
+++ noise_test.go
@@ -35,9 +35,11 @@ func (NoiseSuite) TestN(c *C) {
 		PeerStatic:  staticR.Public,
 	})
 
-	hello, _, _, _ := hs.WriteMessage(nil, nil)
+	out := &SimplePayload{}
+
+	hs.WriteMessage(out, nil)
 	expected, _ := hex.DecodeString("358072d6365880d1aeea329adf9121383851ed21a28e3b75e965d0d2cd1662548331a3d1e93b490263abc7a4633867f4")
-	c.Assert(hello, DeepEquals, expected)
+	c.Assert(out.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) TestX(c *C) {
@@ -54,9 +56,10 @@ func (NoiseSuite) TestX(c *C) {
 		PeerStatic:    staticR.Public,
 	})
 
-	hello, _, _, _ := hs.WriteMessage(nil, nil)
+	out := &SimplePayload{}
+	hs.WriteMessage(out, nil)
 	expected, _ := hex.DecodeString("79a631eede1bf9c98f12032cdeadd0e7a079398fc786b88cc846ec89af85a51ad203cd28d81cf65a2da637f557a05728b3ae4abdc3a42d1cda5f719d6cf41d7f2cf1b1c5af10e38a09a9bb7e3b1d589a99492cc50293eaa1f3f391b59bb6990d")
-	c.Assert(hello, DeepEquals, expected)
+	c.Assert(out.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) TestNN(c *C) {
@@ -78,20 +81,23 @@ func (NoiseSuite) TestNN(c *C) {
 		Initiator:   false,
 	})
 
-	msg, _, _, _ := hsI.WriteMessage(nil, []byte("abc"))
-	c.Assert(msg, HasLen, 35)
-	res, _, _, err := hsR.ReadMessage(nil, msg)
+	in := &SimplePayload{}
+	hsI.WriteMessage(in, []byte("abc"))
+	c.Assert(in.Serialize(), HasLen, 35)
+	var res []byte
+	res, _, _, err := hsR.ReadMessage(res, in)
 	c.Assert(err, IsNil)
 	c.Assert(string(res), Equals, "abc")
 
-	msg, _, _, _ = hsR.WriteMessage(nil, []byte("defg"))
-	c.Assert(msg, HasLen, 52)
-	res, _, _, err = hsI.ReadMessage(nil, msg)
+	in.Reset()
+	hsR.WriteMessage(in, []byte("defg"))
+	c.Assert(in.Serialize(), HasLen, 52)
+	res, _, _, err = hsI.ReadMessage(nil, in)
 	c.Assert(err, IsNil)
 	c.Assert(string(res), Equals, "defg")
 
 	expected, _ := hex.DecodeString("07a37cbc142093c8b755dc1b10e86cb426374ad16aa853ed0bdfc0b2b86d1c7c5e4dc9545d41b3280f4586a5481829e1e24ec5a0")
-	c.Assert(msg, DeepEquals, expected)
+	c.Assert(in.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) TestXX(c *C) {
@@ -116,27 +122,29 @@ func (NoiseSuite) TestXX(c *C) {
 		Pattern:       HandshakeXX,
 		StaticKeypair: staticR,
 	})
-
-	msg, _, _, _ := hsI.WriteMessage(nil, []byte("abc"))
-	c.Assert(msg, HasLen, 35)
-	res, _, _, err := hsR.ReadMessage(nil, msg)
+	out := &SimplePayload{}
+	hsI.WriteMessage(out, []byte("abc"))
+	c.Assert(out.Serialize(), HasLen, 35)
+	var payload []byte
+	payload, _, _, err := hsR.ReadMessage(payload, out)
 	c.Assert(err, IsNil)
-	c.Assert(string(res), Equals, "abc")
+	c.Assert(string(payload), Equals, "abc")
 
-	msg, _, _, _ = hsR.WriteMessage(nil, []byte("defg"))
-	c.Assert(msg, HasLen, 100)
-	res, _, _, err = hsI.ReadMessage(nil, msg)
+	hsR.WriteMessage(out, []byte("defg"))
+	c.Assert(out.Serialize(), HasLen, 100)
+	payload, _, _, err = hsI.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
-	c.Assert(string(res), Equals, "defg")
+	c.Assert(string(payload), Equals, "defg")
 
-	msg, _, _, _ = hsI.WriteMessage(nil, nil)
-	c.Assert(msg, HasLen, 64)
-	res, _, _, err = hsR.ReadMessage(nil, msg)
+	out.Reset()
+	hsI.WriteMessage(out, nil)
+	c.Assert(out.Serialize(), HasLen, 64)
+	payload, _, _, err = hsR.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
-	c.Assert(res, HasLen, 0)
+	c.Assert(payload, HasLen, 0)
 
 	expected, _ := hex.DecodeString("8127f4b35cdbdf0935fcf1ec99016d1dcbc350055b8af360be196905dfb50a2c1c38a7ca9cb0cfe8f4576f36c47a4933eee32288f590ac4305d4b53187577be7")
-	c.Assert(msg, DeepEquals, expected)
+	c.Assert(out.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) TestIK(c *C) {
@@ -165,20 +173,23 @@ func (NoiseSuite) TestIK(c *C) {
 		StaticKeypair: staticR,
 	})
 
-	msg, _, _, _ := hsI.WriteMessage(nil, []byte("abc"))
-	c.Assert(msg, HasLen, 99)
-	res, _, _, err := hsR.ReadMessage(nil, msg)
+	out := &SimplePayload{}
+	hsI.WriteMessage(out, []byte("abc"))
+	c.Assert(out.Serialize(), HasLen, 99)
+	var payload []byte
+	payload, _, _, err := hsR.ReadMessage(payload, out)
 	c.Assert(err, IsNil)
-	c.Assert(string(res), Equals, "abc")
+	c.Assert(string(payload), Equals, "abc")
 
-	msg, _, _, _ = hsR.WriteMessage(nil, []byte("defg"))
-	c.Assert(msg, HasLen, 52)
-	res, _, _, err = hsI.ReadMessage(nil, msg)
+	out.Reset()
+	hsR.WriteMessage(out, []byte("defg"))
+	c.Assert(out.Serialize(), HasLen, 52)
+	payload, _, _, err = hsI.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
-	c.Assert(string(res), Equals, "defg")
+	c.Assert(string(payload), Equals, "defg")
 
 	expected, _ := hex.DecodeString("5869aff450549732cbaaed5e5df9b30a6da31cb0e5742bad5ad4a1a768f1a67b7555a94199d0ce2972e0861b06c2152419a278de")
-	c.Assert(msg, DeepEquals, expected)
+	c.Assert(out.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) TestXXRoundtrip(c *C) {
@@ -205,30 +216,34 @@ func (NoiseSuite) TestXXRoundtrip(c *C) {
 	})
 
 	// -> e
-	msg, _, _, _ := hsI.WriteMessage(nil, []byte("abcdef"))
-	c.Assert(msg, HasLen, 38)
-	res, _, _, err := hsR.ReadMessage(nil, msg)
+	out := &SimplePayload{}
+	hsI.WriteMessage(out, []byte("abcdef"))
+	c.Assert(out.Serialize(), HasLen, 38)
+	var payload []byte
+	payload, _, _, err := hsR.ReadMessage(payload, out)
 	c.Assert(err, IsNil)
-	c.Assert(string(res), Equals, "abcdef")
+	c.Assert(string(payload), Equals, "abcdef")
 
+	out.Reset()
 	// <- e, dhee, s, dhse
-	msg, _, _, _ = hsR.WriteMessage(nil, nil)
-	c.Assert(msg, HasLen, 96)
-	res, _, _, err = hsI.ReadMessage(nil, msg)
+	hsR.WriteMessage(out, nil)
+	c.Assert(out.Serialize(), HasLen, 96)
+	payload, _, _, err = hsI.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
-	c.Assert(res, HasLen, 0)
+	c.Assert(payload, HasLen, 0)
 
 	// -> s, dhse
-	payload := "0123456789012345678901234567890123456789012345678901234567890123456789"
-	msg, csI0, csI1, _ := hsI.WriteMessage(nil, []byte(payload))
-	c.Assert(msg, HasLen, 134)
-	res, csR0, csR1, err := hsR.ReadMessage(nil, msg)
+	out.Reset()
+	expectedPayload := []byte("0123456789012345678901234567890123456789012345678901234567890123456789")
+	csI0, csI1, _ := hsI.WriteMessage(out, expectedPayload)
+	c.Assert(out.Serialize(), HasLen, 134)
+	payload, csR0, csR1, err := hsR.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
-	c.Assert(string(res), Equals, payload)
+	c.Assert(string(expectedPayload), Equals, string(payload))
 
 	// transport message I -> R
-	msg = csI0.Encrypt(nil, nil, []byte("wubba"))
-	res, err = csR0.Decrypt(nil, nil, msg)
+	msg := csI0.Encrypt(nil, nil, []byte("wubba"))
+	res, err := csR0.Decrypt(nil, nil, msg)
 	c.Assert(err, IsNil)
 	c.Assert(string(res), Equals, "wubba")
 
@@ -266,21 +281,22 @@ func (NoiseSuite) Test_NNpsk0_Roundtrip(c *C) {
 	})
 
 	// -> e
-	msg, _, _, _ := hsI.WriteMessage(nil, nil)
-	c.Assert(msg, HasLen, 48)
-	res, _, _, err := hsR.ReadMessage(nil, msg)
+	out := &SimplePayload{}
+	hsI.WriteMessage(out, nil)
+	c.Assert(out.Serialize(), HasLen, 48)
+	res, _, _, err := hsR.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(res, HasLen, 0)
 
 	// <- e, dhee
-	msg, csR0, csR1, _ := hsR.WriteMessage(nil, nil)
-	c.Assert(msg, HasLen, 48)
-	res, csI0, csI1, err := hsI.ReadMessage(nil, msg)
+	csR0, csR1, _ := hsR.WriteMessage(out, nil)
+	c.Assert(out.Serialize(), HasLen, 48)
+	res, csI0, csI1, err := hsI.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(res, HasLen, 0)
 
 	// transport I -> R
-	msg = csI0.Encrypt(nil, nil, []byte("foo"))
+	msg := csI0.Encrypt(nil, nil, []byte("foo"))
 	res, err = csR0.Decrypt(nil, nil, msg)
 	c.Assert(err, IsNil)
 	c.Assert(string(res), Equals, "foo")
@@ -305,12 +321,12 @@ func (NoiseSuite) Test_Npsk0(c *C) {
 		PresharedKey: []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20},
 		PeerStatic:   staticR.Public,
 	})
-
-	msg, _, _, _ := hsI.WriteMessage(nil, nil)
-	c.Assert(msg, HasLen, 48)
+	out := &SimplePayload{}
+	hsI.WriteMessage(out, nil)
+	c.Assert(out.Serialize(), HasLen, 48)
 
 	expected, _ := hex.DecodeString("358072d6365880d1aeea329adf9121383851ed21a28e3b75e965d0d2cd1662542044ae563929068930dcf04674526cb9")
-	c.Assert(msg, DeepEquals, expected)
+	c.Assert(out.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) Test_Xpsk0(c *C) {
@@ -328,11 +344,12 @@ func (NoiseSuite) Test_Xpsk0(c *C) {
 		StaticKeypair: staticI,
 		PeerStatic:    staticR.Public,
 	})
-	msg, _, _, _ := hs.WriteMessage(nil, nil)
-	c.Assert(msg, HasLen, 96)
+	out := &SimplePayload{}
+	hs.WriteMessage(out, nil)
+	c.Assert(out.Serialize(), HasLen, 96)
 
 	expected, _ := hex.DecodeString("79a631eede1bf9c98f12032cdeadd0e7a079398fc786b88cc846ec89af85a51ad51eef529db0dd9127d4aa59a9183e118337d75a4e55e7e00f85c3d20ede536dd0112eec8c3b2a514018a90ab685b027dd24aa0c70b0c0f00524cc23785028b9")
-	c.Assert(msg, DeepEquals, expected)
+	c.Assert(out.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) Test_NNpsk0(c *C) {
@@ -359,20 +376,21 @@ func (NoiseSuite) Test_NNpsk0(c *C) {
 		PresharedKey: psk,
 	})
 
-	msg, _, _, _ := hsI.WriteMessage(nil, []byte("abc"))
-	c.Assert(msg, HasLen, 51)
-	res, _, _, err := hsR.ReadMessage(nil, msg)
+	out := &SimplePayload{}
+	hsI.WriteMessage(out, []byte("abc"))
+	c.Assert(out.Serialize(), HasLen, 51)
+	res, _, _, err := hsR.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(string(res), Equals, "abc")
 
-	msg, _, _, _ = hsR.WriteMessage(nil, []byte("defg"))
-	c.Assert(msg, HasLen, 52)
-	res, _, _, err = hsI.ReadMessage(nil, msg)
+	hsR.WriteMessage(out, []byte("defg"))
+	c.Assert(out.Serialize(), HasLen, 52)
+	res, _, _, err = hsI.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(string(res), Equals, "defg")
 
 	expected, _ := hex.DecodeString("07a37cbc142093c8b755dc1b10e86cb426374ad16aa853ed0bdfc0b2b86d1c7c3e42e140cfffbcdf5d9d2a1c24ce4cdbdf1eaf37")
-	c.Assert(msg, DeepEquals, expected)
+	c.Assert(out.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) Test_XXpsk0(c *C) {
@@ -404,26 +422,28 @@ func (NoiseSuite) Test_XXpsk0(c *C) {
 		StaticKeypair: staticR,
 	})
 
-	msg, _, _, _ := hsI.WriteMessage(nil, []byte("abc"))
-	c.Assert(msg, HasLen, 51)
-	res, _, _, err := hsR.ReadMessage(nil, msg)
+	out := &SimplePayload{}
+	hsI.WriteMessage(out, []byte("abc"))
+	c.Assert(out.Serialize(), HasLen, 51)
+	res, _, _, err := hsR.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(string(res), Equals, "abc")
 
-	msg, _, _, _ = hsR.WriteMessage(nil, []byte("defg"))
-	c.Assert(msg, HasLen, 100)
-	res, _, _, err = hsI.ReadMessage(nil, msg)
+	hsR.WriteMessage(out, []byte("defg"))
+	c.Assert(out.Serialize(), HasLen, 100)
+	res, _, _, err = hsI.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(string(res), Equals, "defg")
 
-	msg, _, _, _ = hsI.WriteMessage(nil, nil)
-	c.Assert(msg, HasLen, 64)
-	res, _, _, err = hsR.ReadMessage(nil, msg)
+	out.Reset()
+	hsI.WriteMessage(out, nil)
+	c.Assert(out.Serialize(), HasLen, 64)
+	res, _, _, err = hsR.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(res, HasLen, 0)
 
 	expected, _ := hex.DecodeString("1b6d7cc3b13bd02217f9cdb98c50870db96281193dca4df570bf6230a603b686fd90d2914c7e797d9276ef8fb34b0c9d87faa048ce4bc7e7af21b6a450352275")
-	c.Assert(msg, DeepEquals, expected)
+	c.Assert(out.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) TestHandshakeRollback(c *C) {
@@ -445,24 +465,25 @@ func (NoiseSuite) TestHandshakeRollback(c *C) {
 		Initiator:   false,
 	})
 
-	msg, _, _, _ := hsI.WriteMessage(nil, []byte("abc"))
-	c.Assert(msg, HasLen, 35)
-	res, _, _, err := hsR.ReadMessage(nil, msg)
+	out := &SimplePayload{}
+	hsI.WriteMessage(out, []byte("abc"))
+	c.Assert(out.Serialize(), HasLen, 35)
+	res, _, _, err := hsR.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(string(res), Equals, "abc")
 
-	msg, _, _, _ = hsR.WriteMessage(nil, []byte("defg"))
-	c.Assert(msg, HasLen, 52)
-	prev := msg[1]
-	msg[1] = msg[1] + 1
-	_, _, _, err = hsI.ReadMessage(nil, msg)
+	hsR.WriteMessage(out, []byte("defg"))
+	c.Assert(out.Serialize(), HasLen, 52)
+	prev := out.epublic[1]
+	out.epublic[1] = out.epublic[1] + 1
+	_, _, _, err = hsI.ReadMessage(nil, out)
 	c.Assert(err, Not(IsNil))
-	msg[1] = prev
-	res, _, _, err = hsI.ReadMessage(nil, msg)
+	out.epublic[1] = prev
+	res, _, _, err = hsI.ReadMessage(nil, out)
 	c.Assert(string(res), Equals, "defg")
 
 	expected, _ := hex.DecodeString("07a37cbc142093c8b755dc1b10e86cb426374ad16aa853ed0bdfc0b2b86d1c7c5e4dc9545d41b3280f4586a5481829e1e24ec5a0")
-	c.Assert(msg, DeepEquals, expected)
+	c.Assert(out.Serialize(), DeepEquals, expected)
 }
 
 func (NoiseSuite) TestRekey(c *C) {
@@ -490,17 +511,18 @@ func (NoiseSuite) TestRekey(c *C) {
 	serverConfig.EphemeralKeypair, _ = DH25519.GenerateKeypair(rng)
 	serverHs, _ := NewHandshakeState(serverConfig)
 
-	clientHsMsg, _, _, _ := clientHs.WriteMessage(nil, nil)
-	c.Assert(32, Equals, len(clientHsMsg))
+	out := &SimplePayload{}
+	clientHs.WriteMessage(out, nil)
+	c.Assert(32, Equals, out.Length())
 
-	serverHsResult, _, _, err := serverHs.ReadMessage(nil, clientHsMsg)
+	serverHsResult, _, _, err := serverHs.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(0, Equals, len(serverHsResult))
 
-	serverHsMsg, csR0, csR1, _ := serverHs.WriteMessage(nil, nil)
-	c.Assert(48, Equals, len(serverHsMsg))
+	csR0, csR1, _ := serverHs.WriteMessage(out, nil)
+	c.Assert(48, Equals, out.Length())
 
-	clientHsResult, csI0, csI1, err := clientHs.ReadMessage(nil, serverHsMsg)
+	clientHsResult, csI0, csI1, err := clientHs.ReadMessage(nil, out)
 	c.Assert(err, IsNil)
 	c.Assert(0, Equals, len(clientHsResult))
 
